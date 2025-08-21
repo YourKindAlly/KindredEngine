326611d05b5cf95db6924d9ace1fbdc5ad236af0
Jasmine Regnér <jasmine.regner@proton.me>
2025-08-21 07:32:12 +0200

    Add movement input actions.

--- game/Player.cpp
+++ game/Player.cpp
@@ -1,21 +1,25 @@
 #include "Player.h"
 #include "../header_files/Sprite.h"
 #include "../header_files/GameWindow.h"
-#include "../header_files/Input.h"
 
 Player::Player(GameWindow* window) : Object(window) {
     transform = Transform{ Vector2(300, 200) };
 
-    const auto sprite = window->Create_Render_Object<Sprite>("../assets/ship.png");
+    auto sprite = window->Create_Render_Object<Sprite>("../assets/ship.png");
     sprite->parent = this;
     sprite->Set_Position(transform.position);
     children.push_back(sprite);
 
-    const auto input = window->Create_Object<Input>();
+    auto input = window->Create_Object<Input>();
     input->parent = this;
     children.push_back(input);
+
+    this->input = input;
 }
 
 void Player::Update() {
-
+    input->Create_Action("move_up", SDLK_W);
+    input->Create_Action("move_down", SDLK_S);
+    input->Create_Action("move_left", SDLK_A);
+    input->Create_Action("move_right", SDLK_D);
 }
--- game/Player.h
+++ game/Player.h
@@ -2,12 +2,15 @@
 #define SDL3TEST_PLAYER_H
 
 #include "../header_files/Object.h"
+#include "../header_files/Input.h"
 
 class Player final : public Object {
 public:
     explicit Player(GameWindow* window);
 
     void Update() override;
+
+    Input* input = nullptr;
 };
 
 
